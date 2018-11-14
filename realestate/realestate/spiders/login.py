4ee6301f8be8724fcb5da326304b3f45576b5d0c
Wang Yang <khil.wang@gmail.com>
2018-11-14 16:51:05 +0800

    update

--- realestate/realestate/spiders/login.py
+++ realestate/realestate/spiders/login.py
@@ -1,43 +1,70 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+class House(scrapy.Item):
+    address = scrapy.Field()
+    price = scrapy.Field()
+    property_type = scrapy.Field()
+    bed = scrapy.Field()
+    bath = scrapy.Field()
+    carPark = scrapy.Field()
+    land_size = scrapy.Field()
+    land_unit = scrapy.Field()
+    detail_link = scrapy.Field()
+    statement_url = scrapy.Field()
+
 class LoginSpider(scrapy.Spider):
     name = 'login'
     allowed_domains = ['www.realestate.com.au']
-    start_urls = ['https://www.realestate.com.au/buy/in-wa/list-1']
+    start_urls = ['https://www.realestate.com.au/buy/in-vic/list-1']
 
     def parse(self, response):
         for estate in response.xpath('//*[@class="residential-card__content"]'):
 
+            house = House()
+
             price = estate.xpath('div[1]/span/text()').extract_first()
+            house['price'] = price
 
             address = estate.xpath('div[2]/div/h2/a/span/text()').extract_first()
+            house['address'] = address
+
             property_type = estate.xpath('div[2]/p/span/text()').extract_first()
+            house['property_type'] = property_type
 
             feature_count = len(estate.xpath('div[3]/ul/li'))
             bed = 0
             bath = 0
-            car = 0
+            carPark = 0
             if feature_count > 0:
                 bed = estate.xpath('div[3]/ul/li[1]/span[1]/text()')[1].extract()
+                house['bed'] = bed 
             if feature_count > 1:
                 bath = estate.xpath('div[3]/ul/li[2]/span[1]/text()')[1].extract()
+                house['bath'] = bath
             if feature_count > 2:
-                car = estate.xpath('div[3]/ul/li[3]/span[1]/text()')[1].extract()
+                carPark = estate.xpath('div[3]/ul/li[3]/span[1]/text()')[1].extract()
+                house['carPark'] = carPark
 
             land_count = len(estate.xpath('div[3]/div'))
             land_size = 0
             land_unit = None
             if land_count > 0:
                 land_size = estate.xpath('div[3]/div/span/text()').extract()[1]
+                house['land_size'] = land_size
                 land_unit = estate.xpath('div[3]/div/span/text()').extract()[3]
+                house['land_unit'] = land_unit
 
             detail_link = estate.xpath('../..//*[@class="details-link "]/@href').extract_first()
             detail_link = response.urljoin(detail_link)
+            house['detail_link'] = detail_link
             request = scrapy.Request(detail_link, callback=self.parse_details)
-            
 
-            print price, address, property_type, bed, bath, car, land_size, land_unit, detail_link
+            request.meta['item'] = house
+            yield request
+            
+            #statement_url = house['statement_url']
+            #print price, address, property_type, bed, bath, carPark, land_size, land_unit, detail_link
 
         next_page = response.xpath('//*[@class="pagination__next"]/a/@href').extract_first()
 
@@ -48,16 +75,7 @@ class LoginSpider(scrapy.Spider):
     @staticmethod
     def parse_details(response):
         house = response.meta['item']
-        primary_content = response.css("div#detailsCont #primaryContent")
-        house['title'] = primary_content.css("p.title::text").extract_first()
-        house['description'] = primary_content.css("p.body::text").extract()
-        other_features = []
-        features = response.css("div#detailsCont #primaryContent #features div.featureList").css("ul li")
-        for li in features:
-            other1 = OtherFeatures()
-            other1['name'] = li.css("::text").extract_first()
-            other1['description'] = li.css("span::text").extract_first()
-            other_features.append(other1)
-
-        house['otherFeatures'] = other_features
+        statement_url = response.xpath('//*[@class="statement-of-information__link"]/@href').extract_first() 
+        if statement_url != None:
+            house['statement_url'] = statement_url
         yield house
